ddef499dabcb8bd57a90badcf062315f1e10e601
Erick <fis9.81@gmail.com>
2024-05-13 18:15:54 -0400

    finished sidebar

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { routes } from '../../app.routes';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-sidemenu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterModule],
   templateUrl: './sidemenu.component.html',
   styleUrl: './sidemenu.component.scss'
 })
 export class SidemenuComponent {
+  public menuItems = routes
+    .map(route => route.children ?? [])
+    .flat()
+    .filter(route => route && route.path)
+    .filter(route => !route.path?.includes(":"))
+  constructor() {
+  }
 
 }
