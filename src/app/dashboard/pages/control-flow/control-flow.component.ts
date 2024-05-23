9d48251b8c1243a24175be58ef8f7895789a1b54
Erick <fis9.81@gmail.com>
2024-05-23 16:05:31 -0400

    we added the section about control flow

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
+type Grade = 'a' | 'b' | 'f'
 @Component({
   selector: 'app-control-flow',
   standalone: true,
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './control-flow.component.scss'
 })
 export default class ControlFlowComponent {
-
+  public showContent = signal(false);
+  public grade = signal<Grade>('a')
+  public frameworks = signal(['Angular', 'Vue', 'Svelte', 'Qwik', 'React'])
+  public frameworks2 = signal([])
+  public toggleContent() {
+    this.showContent.update(update => !update)
+  }
 }
