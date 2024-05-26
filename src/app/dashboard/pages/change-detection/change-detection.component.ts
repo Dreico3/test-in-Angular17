a8acfc4c6ce1858f69da6cc9f86345904fd1fa85
Erick <fis9.81@gmail.com>
2024-05-26 19:04:36 -0400

    modificated change-detection.component with two examples the first with a signal an the second with a normal parameter

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,13 +1,36 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   selector: 'app-change-detection',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TitleComponent],
   templateUrl: './change-detection.component.html',
-  styleUrl: './change-detection.component.scss'
+  styleUrl: './change-detection.component.scss',
+  changeDetection:ChangeDetectionStrategy.OnPush
 })
 export default class ChangeDetectionComponent {
-
+  public currentFramework = computed(
+    ()=>`Change detection ${this.frameworkAsSignal().name}`
+  )
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016
+  })
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016
+  }
+  constructor(){
+    setTimeout(()=>{
+      
+      this.frameworkAsProperty.name="react"
+      this.frameworkAsSignal.update(value=>({
+        ...value,
+        name:"React"
+      }))
+      console.log("hecho")
+    },3000)
+  }
 }
